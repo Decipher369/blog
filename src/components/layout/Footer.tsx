3b775f03275a4abaf7b8085dd0105ef37522abc8
Decipher <karthigesonshagash2005@gmail.com>
2026-02-01 16:59:01 +0530

    Update src/components/layout/Footer.tsx
    Co-authored-by: Janitha Gamage <janithasuranjana2001@gmail.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-50">
+    <footer className="bg-gray-50 border-t border-gray-200">
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
 
         {/* Bottom Bar: Copyright */}
